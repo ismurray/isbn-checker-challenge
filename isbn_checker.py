1da73746d4de61e40c3ae17f68b6b63b442b3b4f
Ian Murray <ian@gamersensei.com>
2019-05-03 02:41:27 -0400

    Add tests and logic for ISBN-13

--- isbn_checker.py
+++ isbn_checker.py
@@ -3,12 +3,19 @@ import re
 class IsbnChecker:
     def __init__(self, isbn):
         self.isbn = self.__sanitize_input(isbn)
-        # self.isbn_type = isbn.length == 10 ? 'isbn-10' : 'isbn-13'
 
     def check(self):
-        if len(self.isbn) != 10:
+        if len(self.isbn) == 10:
+            return self.__check_10()
+        elif len(self.isbn) == 13:
+            return self.__check_13()
+        else:
             return False
 
+    def __sanitize_input(self, isbn_input):
+        return re.sub('[\s+-]', '', isbn_input)
+
+    def __check_10(self):
         sum = 0
         if self.isbn.endswith('X'):
             sum = 100
@@ -21,6 +28,16 @@ class IsbnChecker:
 
         return sum % 11 == 0
 
+    def __check_13(self):
+        sum = 0
 
-    def __sanitize_input(self, isbn_input):
-        return re.sub('[\s+-]', '', isbn_input)
+        for i in range(len(self.isbn)):
+            if not self.isbn[i].isdigit():
+                return False
+
+            if (i + 1) % 2 == 1:
+                sum += int(self.isbn[i])
+            elif (i + 1) % 2 == 0:
+                sum += int(self.isbn[i]) * 3
+
+        return (10 - (sum % 10)) % 10 == 0
--- isbn_checker_test.py
+++ isbn_checker_test.py
@@ -1,23 +1,41 @@
 import pytest
 from isbn_checker import IsbnChecker
 
-def test_valid_isbn_ten_checksum():
+def test_valid_isbn_10_checksum():
     assert IsbnChecker('0471958697').check() == True
 
-def test_valid_isbn_ten_with_trailing_x():
+def test_valid_isbn_10_with_trailing_x():
     assert IsbnChecker('097522980X').check() == True
 
-def test_valid_isbn_ten_with_spaces():
+def test_valid_isbn_10_with_spaces():
     assert IsbnChecker('0 471 60695 2').check() == True
 
-def test_valid_isbn_ten_with_dashes():
+def test_valid_isbn_10_with_dashes():
     assert IsbnChecker('0-471-60695-2').check() == True
 
-def test_isbn_ten_with_invalid_checksum():
+def test_isbn_10_with_invalid_checksum():
     assert IsbnChecker('0471958699').check() == False
 
-def test_isbn_ten_with_invalid_length():
+def test_isbn_10_with_invalid_length():
     assert IsbnChecker('04719586977').check() == False
 
-def test_isbn_ten_with_invalid_characters():
+def test_isbn_10_with_invalid_characters():
     assert IsbnChecker('04719X8697').check() == False
+
+def test_valid_isbn_13_checksum():
+    assert IsbnChecker('9780470059029').check() == True
+
+def test_valid_isbn_13_with_spaces():
+    assert IsbnChecker('978 0 471 48648 0').check() == True
+
+def test_valid_isbn_13_with_dashes():
+    assert IsbnChecker('978-0-13-149505-0').check() == True
+
+def test_isbn_13_with_invalid_checksum():
+    assert IsbnChecker('9780470059021').check() == False
+
+def test_isbn_13_with_invalid_length():
+    assert IsbnChecker('978047005902999').check() == False
+
+def test_isbn_13_with_invalid_characters():
+    assert IsbnChecker('978047005902X').check() == False
